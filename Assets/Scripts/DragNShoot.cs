ae6819077235f07f01a600008dc1c8f217cf3010
Romeq07 <rob.zaplata@gmail.com>
2020-05-27 13:23:45 +0200

    Dodany Slingshot mechanism.

---
+++ Assets/Scripts/DragNShoot.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragNShoot : MonoBehaviour
+{
+    public float power;
+    public Rigidbody playerRb;
+    public Vector3 minPower;
+    public Vector3 maxPower;
+    Camera mainCamera;
+    Vector3 force;
+    Vector3 startPoint;
+    Vector3 endPoint;
+ 
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            startPoint = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, mainCamera.transform.position.z)) * -1;
+            Debug.Log(startPoint);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            endPoint.y = 0f;
+            endPoint = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, mainCamera.transform.position.z)) * -1;
+            Debug.Log(endPoint);
+
+            force = new Vector3(Mathf.Clamp(startPoint.x - endPoint.x, minPower.x, maxPower.x), 0.0f, Mathf.Clamp(startPoint.z - endPoint.z, minPower.z, maxPower.z));
+            playerRb.AddForce(force * power, ForceMode.Impulse);
+        }
+    }
+}
---
+++ Assets/Scripts/SlingShot.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlingShot : MonoBehaviour
+{
+    bool isPressed;
+    Rigidbody playerRb;
+    public GameObject sling;
+    float playerPosition;
+    public float maxDragDistance;
+    Rigidbody slingRb;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerPosition = transform.position.z;
+        slingRb = sling.GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        if (isPressed)
+        {
+            DragBall();
+        }
+
+    }
+
+    private void DragBall()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        float dist = playerRb.position.z - Camera.main.transform.position.z;
+        mousePosition.z = dist + 1.485f;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        mousePosition.y = playerRb.position.y;
+
+        float dragDistance = Vector3.Distance(mousePosition, sling.transform.position);
+
+        if(dragDistance > maxDragDistance)
+        {
+            Vector3 direction = (mousePosition - sling.transform.position).normalized;
+            playerRb.position = sling.transform.position + direction * maxDragDistance;
+        }
+        else
+        {
+            playerRb.position = mousePosition;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        isPressed = true;
+        playerRb.isKinematic = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isPressed = false;
+        playerRb.isKinematic = false;
+        StartCoroutine(ExecuteAfterTime(0.01f));
+    }
+
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        sling.gameObject.SetActive(false);
+    }
+
+    private void DisableSling()
+    {
+        sling.gameObject.SetActive(false);
+    }
+}
